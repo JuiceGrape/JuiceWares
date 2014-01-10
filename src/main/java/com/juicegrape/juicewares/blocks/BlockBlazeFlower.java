3e97994054c4b6d11d91c53063a13fcb68b17caf
JuiceGrape <bmentjot@gmail.com>
2014-01-10 01:40:57 +0100

    BlazeFlower tweaks

--- src/main/java/com/juicegrape/juicewares/blocks/BlockBlazeFlower.java
+++ src/main/java/com/juicegrape/juicewares/blocks/BlockBlazeFlower.java
@@ -3,6 +3,7 @@ package com.juicegrape.juicewares.blocks;
 import java.util.ArrayList;
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockNetherStalk;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
@@ -39,13 +40,33 @@ public class BlockBlazeFlower extends BlockNetherStalk {
 		
 		int meta = world.getBlockMetadata(x, y, z);
 		
+		if (world.isRaining() && !checkBlockAbove(world, x, y + 1, z)) {
+			this.dropBlockAsItem(world, x, y, z, meta, 0);
+			world.setBlockToAir(x, y, z);
+		}
+		
+		
 		if (meta < 15) {
 			int chance = random.nextInt(10);
-			if (chance == 0) {
+			if (world.getBlockId(x, y - 2, z) == Block.lavaStill.blockID) {
+				world.setBlockMetadataWithNotify(x, y, z, meta + 1, 4);
+				if (random.nextInt(100) == 0 ) {
+					world.setBlock(x, y - 2, z, Block.obsidian.blockID);
+				}
+			} else if (chance == 0) {
 				world.setBlockMetadataWithNotify(x, y, z, meta + 1, 4);
 			}
 		}
+
 	}
+	
+	 @Override
+	    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+		 if (random.nextInt(10) == 0) {
+		    	spawnParticles("flame", world, x, y, z, random);
+		 }
+
+	    }
 	
 	@Override
 	public Icon getIcon(int side, int meta) {
@@ -53,29 +74,39 @@ public class BlockBlazeFlower extends BlockNetherStalk {
 		case 0:
 		case 1:
 		case 2:
-			return iconArray[0];
 		case 3:
+			return iconArray[0];
 		case 4:
 		case 5:
-			return iconArray[1];
 		case 6:
 		case 7:
+			return iconArray[1];
 		case 8:
-			return iconArray[2];
 		case 9:
 		case 10:
 		case 11:
-			return iconArray[3];
+			return iconArray[2];
 		case 12:
 		case 13:
-		case 14:	
+		case 14:
+			return iconArray[3];
+		case 15:	
 			return iconArray[4];
-		case 15:
-			return iconArray[5];
 		default:
 			return iconArray[4];
 		}
 	}
+	
+    protected void spawnParticles(String particles, World world, int x, int y, int z, Random random) {
+    		float particleX = x + random.nextFloat();
+    		float particleY = y + random.nextFloat();
+    		float particleZ = z + random.nextFloat();
+    		float particleMotionX = 0F;
+    		float particleMotionY = 0F;
+    		float particleMotionZ = 0F;
+    		
+    		world.spawnParticle(particles, particleX, particleY, particleZ, particleMotionX, particleMotionY, particleMotionZ);
+    }
 	
 	@Override
 	public ArrayList<ItemStack> getBlockDropped(World world, int x, int y, int z, int metadata, int fortune) {
@@ -83,7 +114,7 @@ public class BlockBlazeFlower extends BlockNetherStalk {
 		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
 		
 		if (metadata >= 15) {
-			ret.add(new ItemStack(Items.blazeflowerseeds, 1 + random.nextInt(1), 0));
+			ret.add(new ItemStack(Items.blazeflowerseeds, 1 + random.nextInt(2), 0));
 			ret.add(new ItemStack(Item.blazeRod, 1, 0));
 			ret.add(new ItemStack(Item.blazePowder, random.nextInt(2), 0));
 		} else {
@@ -96,13 +127,22 @@ public class BlockBlazeFlower extends BlockNetherStalk {
 	
 	@Override
 	public void registerIcons(IconRegister register) {
-		iconArray = new Icon[6];
+		iconArray = new Icon[5];
 		
 		for (int i = 0; i < iconArray.length; i++) {
-			iconArray[i] = register.registerIcon(BlockInfo.TEXTURE_LOCATION + ":" + BlockInfo.BLAZEFLOWER + i);
+			iconArray[i] = register.registerIcon(BlockInfo.TEXTURE_LOCATION + ":blazeflower/" + BlockInfo.BLAZEFLOWER + i);
 		}
 		
 		
+	}
+	
+	private boolean checkBlockAbove(World world, int x, int y, int z) {
+		for (int i = 0; i < 128; i++) {
+			if (!world.isAirBlock(x, y + i, z)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
--- src/main/java/com/juicegrape/juicewares/network/EventHooks.java
+++ src/main/java/com/juicegrape/juicewares/network/EventHooks.java
@@ -1,11 +1,18 @@
 package com.juicegrape.juicewares.network;
 
+import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 
 import com.juicegrape.juicewares.items.Items;
 import com.juicegrape.juicewares.potionEffects.Potions;
@@ -21,6 +28,20 @@ public class EventHooks {
 		
 		customPotionEffectHandler(event.entityLiving);
 		
+	}
+	
+	@ForgeSubscribe
+	public void onBlockBreak(BreakEvent event) {
+		if (event.block.blockID == Block.mobSpawner.blockID) {
+			if (event.world.getBlockTileEntity(event.x, event.y, event.z) instanceof TileEntityMobSpawner) {
+				TileEntityMobSpawner te = (TileEntityMobSpawner)event.world.getBlockTileEntity(event.x, event.y, event.z);
+				
+				 if (te.getSpawnerLogic().getEntityNameToSpawn().equals("Blaze")) {
+					 event.world.spawnEntityInWorld(createItem(event.world, new ItemStack(Items.blazeflowerseeds), new Random(), event.x, event.y, event.z));
+				 }
+			}
+
+		}
 	}
 	
 	public void divingHelmetHandler(EntityLivingBase entityLivingBase) {
@@ -76,6 +97,17 @@ public class EventHooks {
 		if (entityLivingBase.isPotionActive(Potions.cWaterBreathing)) {
 			entityLivingBase.setAir(300);
 		}
+	}
+	
+	private EntityItem createItem(World world, ItemStack itemStack, Random random, int x, int y, int z) {
+		float xThang = random.nextFloat() * 0.8F + 0.1F;
+		float yThang = random.nextFloat() * 0.8F + 0.1F;
+		float zThang = random.nextFloat() * 0.8F + 0.1F;
+		EntityItem entityItem = new EntityItem(world, x + xThang, y + yThang, z + zThang, itemStack);
+        entityItem.motionX = (float) random.nextGaussian() * 0.05F;
+        entityItem.motionY = (float) random.nextGaussian() * 0.05F + 0.2F;
+        entityItem.motionZ = (float) random.nextGaussian() * 0.05F;
+        return entityItem;
 	}
 	
 	
